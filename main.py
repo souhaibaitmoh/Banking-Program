7492b8b196d49b0365144263c28158c155fd6258
souhaibaitmoh <souhaib.aitmoh@gmail.com>
2025-02-02 18:20:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,3 @@
-<<<<<<< Updated upstream
-def show_balance(balance):
-    print("*******************************")
-    print(f"Your balance is ${balance:.2f}")
-    print("*******************************")
-
-def deposit():
-    print("*******************************")
-    amount = float(input("Enter an amount to be deposited: "))
-    print("*******************************")
-    
-    if amount < 0:
-        print("*******************************")
-        print("Thats not a valid amount")
-        print("*******************************")
-        return 0
-    else:
-        return amount    
-
-def withdraw(balance):
-    print("*******************************")
-    amount = float(input("Enter an amount to be withdrawn: "))
-    print("*******************************")
-    
-    if amount > balance:
-        print("*******************************")
-        print("Insufficient funds")
-        print("*******************************")
-        return 0
-    elif amount < 0:
-        print("*******************************")
-        print("Amount must be greater than 0") 
-        print("*******************************")
-        return 0
-    else:
-        print("*******************************")
-        print("Withdrawn Successful")
-        print("*******************************")
-        return amount       
-def main():
-    balance = 0
-    is_running = True
-
-    while is_running:
-        print("*******************************")
-        print("    Banking Program    ")
-        print("*******************************")
-        print("1.Show Balance")
-        print("2.Deposit")
-        print("3.Withdraw")
-        print("4.Exit")
-        print("*******************************")
-        choice = input(" Enter your choice (1-4): ")
-        
-        if choice == '1':
-            show_balance(balance)
-        elif choice == '2':
-            balance += deposit()
-        elif choice == '3':
-            balance -= withdraw(balance)
-        elif choice == '4':
-            is_running = False
-        else:
-            print("*******************************")
-            print("That is not a valid choice")  
-            print("*******************************")
-            
-    print("*******************************")        
-    print("Thank you! Have a nice day!")
-    print("*******************************")                      
-    
-
-if __name__ == '__main__':
-=======
 def show_balance(balance):
     print("*******************************")
     print(f"Your balance is ${balance:.2f}")
@@ -157,5 +83,4 @@ def main():
     
 
 if __name__ == '__main__':
->>>>>>> Stashed changes
     main()    
